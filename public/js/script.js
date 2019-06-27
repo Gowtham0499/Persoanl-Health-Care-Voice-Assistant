b6c27c8ee164fd0e10e449a883705983cc0eaa8c
Gowtham0499 <gowth0499@gmail.com>
2019-06-27 19:58:59 +0530

    add all files

---
+++ public/js/script.js
@@ -0,0 +1,232 @@
+'use strict';
+//select language from list of languages
+var langs =
+[['Afrikaans',       ['af-ZA']],
+ ['አማርኛ',           ['am-ET']],
+ ['Azərbaycanca',    ['az-AZ']],
+ ['বাংলা',            ['bn-BD', 'বাংলাদেশ'],
+                     ['bn-IN', 'ভারত']],
+ ['Bahasa Indonesia',['id-ID']],
+ ['Bahasa Melayu',   ['ms-MY']],
+ ['Català',          ['ca-ES']],
+ ['Čeština',         ['cs-CZ']],
+ ['Dansk',           ['da-DK']],
+ ['Deutsch',         ['de-DE']],
+ ['English',         ['en-AU', 'Australia'],
+                     ['en-CA', 'Canada'],
+                     ['en-IN', 'India'],
+                     ['en-KE', 'Kenya'],
+                     ['en-TZ', 'Tanzania'],
+                     ['en-GH', 'Ghana'],
+                     ['en-NZ', 'New Zealand'],
+                     ['en-NG', 'Nigeria'],
+                     ['en-ZA', 'South Africa'],
+                     ['en-PH', 'Philippines'],
+                     ['en-GB', 'United Kingdom'],
+                     ['en-US', 'United States']],
+ ['Español',         ['es-AR', 'Argentina'],
+                     ['es-BO', 'Bolivia'],
+                     ['es-CL', 'Chile'],
+                     ['es-CO', 'Colombia'],
+                     ['es-CR', 'Costa Rica'],
+                     ['es-EC', 'Ecuador'],
+                     ['es-SV', 'El Salvador'],
+                     ['es-ES', 'España'],
+                     ['es-US', 'Estados Unidos'],
+                     ['es-GT', 'Guatemala'],
+                     ['es-HN', 'Honduras'],
+                     ['es-MX', 'México'],
+                     ['es-NI', 'Nicaragua'],
+                     ['es-PA', 'Panamá'],
+                     ['es-PY', 'Paraguay'],
+                     ['es-PE', 'Perú'],
+                     ['es-PR', 'Puerto Rico'],
+                     ['es-DO', 'República Dominicana'],
+                     ['es-UY', 'Uruguay'],
+                     ['es-VE', 'Venezuela']],
+ ['Euskara',         ['eu-ES']],
+ ['Filipino',        ['fil-PH']],
+ ['Français',        ['fr-FR']],
+ ['Basa Jawa',       ['jv-ID']],
+ ['Galego',          ['gl-ES']],
+ ['ગુજરાતી',           ['gu-IN']],
+ ['Hrvatski',        ['hr-HR']],
+ ['IsiZulu',         ['zu-ZA']],
+ ['Íslenska',        ['is-IS']],
+ ['Italiano',        ['it-IT', 'Italia'],
+                     ['it-CH', 'Svizzera']],
+ ['ಕನ್ನಡ',             ['kn-IN']],
+ ['ភាសាខ្មែរ',          ['km-KH']],
+ ['Latviešu',        ['lv-LV']],
+ ['Lietuvių',        ['lt-LT']],
+ ['മലയാളം',          ['ml-IN']],
+ ['मराठी',             ['mr-IN']],
+ ['Magyar',          ['hu-HU']],
+ ['ລາວ',              ['lo-LA']],
+ ['Nederlands',      ['nl-NL']],
+ ['नेपाली भाषा',        ['ne-NP']],
+ ['Norsk bokmål',    ['nb-NO']],
+ ['Polski',          ['pl-PL']],
+ ['Português',       ['pt-BR', 'Brasil'],
+                     ['pt-PT', 'Portugal']],
+ ['Română',          ['ro-RO']],
+ ['සිංහල',          ['si-LK']],
+ ['Slovenščina',     ['sl-SI']],
+ ['Basa Sunda',      ['su-ID']],
+ ['Slovenčina',      ['sk-SK']],
+ ['Suomi',           ['fi-FI']],
+ ['Svenska',         ['sv-SE']],
+ ['Kiswahili',       ['sw-TZ', 'Tanzania'],
+                     ['sw-KE', 'Kenya']],
+ ['ქართული',       ['ka-GE']],
+ ['Հայերեն',          ['hy-AM']],
+ ['தமிழ்',            ['ta-IN', 'இந்தியா'],
+                     ['ta-SG', 'சிங்கப்பூர்'],
+                     ['ta-LK', 'இலங்கை'],
+                     ['ta-MY', 'மலேசியா']],
+ ['తెలుగు',           ['te-IN']],
+ ['Tiếng Việt',      ['vi-VN']],
+ ['Türkçe',          ['tr-TR']],
+ ['اُردُو',            ['ur-PK', 'پاکستان'],
+                     ['ur-IN', 'بھارت']],
+ ['Ελληνικά',         ['el-GR']],
+ ['български',         ['bg-BG']],
+ ['Pусский',          ['ru-RU']],
+ ['Српски',           ['sr-RS']],
+ ['Українська',        ['uk-UA']],
+ ['한국어',            ['ko-KR']],
+ ['中文',             ['cmn-Hans-CN', '普通话 (中国大陆)'],
+                     ['cmn-Hans-HK', '普通话 (香港)'],
+                     ['cmn-Hant-TW', '中文 (台灣)'],
+                     ['yue-Hant-HK', '粵語 (香港)']],
+ ['日本語',           ['ja-JP']],
+ ['हिन्दी',             ['hi-IN']],
+ ['ภาษาไทย',         ['th-TH']]];
+
+for (var i = 0; i < langs.length; i++) {
+  select_language.options[i] = new Option(langs[i][0], i);
+}
+// Set default language / dialect.
+select_language.selectedIndex = 45;
+updateCountry();
+select_dialect.selectedIndex = 0;
+
+function updateCountry() {
+  for (var i = select_dialect.options.length - 1; i >= 0; i--) {
+    select_dialect.remove(i);
+  }
+  var list = langs[select_language.selectedIndex];
+  for (var i = 1; i < list.length; i++) {
+    select_dialect.options.add(new Option(list[i][1], list[i][0]));
+  }
+  select_dialect.style.visibility = list[1].length == 1 ? 'hidden' : 'visible';
+}
+
+//open socket from transfering information 
+const socket = io();
+
+let outputYou = document.querySelector('.output-you');
+let outputBot = document.querySelector('.output-bot');
+
+var speechtext = "";
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+document.querySelector('button').addEventListener('click', () => {
+  recognition.lang = select_dialect.value;
+  recognition.start();
+});
+
+recognition.addEventListener('speechstart', () => {
+  console.log('Speech has been detected.');
+});
+
+recognition.addEventListener('result', (e) => {
+  console.log('Result has been detected.');
+
+  let last = e.results.length - 1;
+  let text = e.results[last][0].transcript;
+
+  speechtext = text;
+
+  outputYou.textContent = text;
+  console.log('Confidence: ' + e.results[0][0].confidence);
+
+  //User input translation
+  if (recognition.lang.substring(0, 2) == "en"){
+    socket.emit('chat message', text);
+  }
+  else{
+  var json = "";
+  var url = "https://translation.googleapis.com/language/translate/v2?key=AIzaSyALVDyli35OvnGJtXHoc1-0Qj5KmwCXgQs";
+  url += "&source=" + recognition.lang.substring(0, 2);
+  url += "&target=en";
+  url += "&q=" + speechtext;
+  $.get(url, function (data, status) {
+         json = JSON.stringify(data.data.translations[0].translatedText);
+         socket.emit('chat message', json);
+         console.log(json);
+  });
+  console.log(url);
+}
+});
+
+recognition.addEventListener('speechend', () => {
+  recognition.stop();
+}); 
+
+recognition.addEventListener('error', (e) => {
+  outputBot.textContent = 'Error: ' + e.error;
+});
+
+
+//bot speak function
+function synthVoice(text) {
+  const synth = window.speechSynthesis;
+  const utterance = new SpeechSynthesisUtterance();
+  utterance.lang = "en-IN";
+  utterance.text = text;
+  synth.speak(utterance);
+  //var json2 = "";
+  //var url2 = "http://translate.google.com/translate_tts?";
+  //url2 += "tl=" + recognition.lang.substring(0, 2);
+  //url2 += "&q=" + text;
+  //url2 += "client=gtx";
+  //$.get(url2, function (data, status) {
+     //json2 = JSON.stringify
+     //console.log("Success Speech");
+     //console.log(url2);
+  //});
+}
+
+
+
+//response to the user translation
+socket.on('bot reply', function(replyText) {
+  if (recognition.lang.substring(0, 2) == "en"){
+    synthVoice(replyText);
+    if(replyText == '') replyText = '(No answer...)';
+         outputBot.textContent = replyText;
+  }
+  else{
+  var json1 = "";
+  var url1 = "https://translation.googleapis.com/language/translate/v2?key=AIzaSyALVDyli35OvnGJtXHoc1-0Qj5KmwCXgQs";
+  url1 += "&source=en";
+  url1 += "&target=" + recognition.lang.substring(0, 2);
+  url1 += "&q=" + replyText;
+  $.get(url1, function (data, status) {
+         json1 = JSON.stringify(data.data.translations[0].translatedText);
+         synthVoice(json1);
+         console.log(json1);
+         if(replyText == '') replyText = '(No answer...)';
+         outputBot.textContent = json1;
+  });
+  //synthVoice(replyText);
+  console.log(url1);
+  //if(replyText == '') replyText = '(No answer...)';
+  //outputBot.textContent = json1;
+}
+});
